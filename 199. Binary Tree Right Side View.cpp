6fda402a2aaaa4aac9236effbe66b44ed70fa1b2
Aman-Arcanion9 <46062838+Aman-Arcanion9@users.noreply.github.com>
2020-06-09 17:58:56 +0530

    better approach

--- 199. Binary Tree Right Side View.cpp
+++ 199. Binary Tree Right Side View.cpp
@@ -14,15 +14,13 @@ private:
     queue<TreeNode *> q;
 public:
     vector<int> rightSideView(TreeNode* root) {
-        
-        vector<vector<int> > ans;
+
         vector<int> res;
         if(root==NULL) return res;
         
         q.push(root);
         q.push(NULL);
-        
-        vector<int> lev;
+        int prev;
         
         while(!q.empty()){
             
@@ -30,22 +28,16 @@ public:
             q.pop();
             
             if(!x){
-                ans.push_back(lev);
-                lev.clear();
-                
+                res.push_back(prev);
                 if(q.size()>0) q.push(NULL);    
             }
             else{
-                lev.push_back(x->val);
+                prev = x->val;
                 if(x->left) q.push(x->left);
                 if(x->right) q.push(x->right);
             }
         }
-        
-        for(int i=0 ; i<ans.size(); i++){
-            int x = ans[i][ans[i].size()-1];
-            res.push_back(x);
-        }
+
         return res;
     }
 };
