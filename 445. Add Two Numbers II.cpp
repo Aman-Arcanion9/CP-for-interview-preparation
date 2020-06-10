943f41a701ee7901a1cd1744cbdf000cfaabbcf2
Aman-Arcanion9 <46062838+Aman-Arcanion9@users.noreply.github.com>
2020-06-10 18:30:12 +0530

    keep in mind if(NULL)

---
+++ 445. Add Two Numbers II.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+        stack<int> s1,s2;
+        
+        while(l1){
+            s1.push(l1->val);
+            l1 = l1->next;
+        }
+        
+        while(l2){
+            s2.push(l2->val);
+            l2 = l2->next;
+        }
+        
+        int carr=0;
+        ListNode *prev = NULL;
+        
+        while(s1.size() || s2.size() || carr){
+            
+            int sum = carr;
+            
+            if(s1.size()){
+                sum += s1.top();
+                s1.pop();
+            }
+            
+            if(s2.size()){
+                sum += s2.top();
+                s2.pop();
+            }
+            
+            ListNode* temp = new ListNode;
+            temp->val = sum%10;
+            carr = sum/10;
+
+            temp->next = prev;
+            prev = temp;
+        }
+        
+        return prev;
+    }
+};
