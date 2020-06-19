f333e5bb2d7225320f848cc538a3eeac49e6a999
Aman-Arcanion9 <46062838+Aman-Arcanion9@users.noreply.github.com>
2020-06-19 23:23:05 +0530

    Copycat

---
+++ 1386. Cinema Seat Allocation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
+        
+        int ans = 0;
+        
+        // Holds the Allocated seats
+        unordered_map<int, vector<int> > alloc;
+        
+        for(auto it:reservedSeats)
+            alloc[it[0]].push_back(it[1]);
+        
+        for(auto it:alloc){
+            
+            int seats = 0;
+            vector<int> row(11,1);      //Seats are indexed from 1-10
+            
+            for(auto j:it.second)
+                row[j] = 0;
+            
+            if(row[2]&&row[3]&&row[4]&&row[5])
+                seats++;
+            if(row[6]&&row[7]&&row[8]&&row[9])
+                seats++;
+            
+            if(!seats){
+                if(row[4]&&row[5]&&row[6]&&row[7])
+                seats++;
+            }
+            
+            // No. of seats in rows where people can't sit
+            ans+=(2-seats);
+        }
+        //Total groups of 4 present - ans                             
+        return 2*n-ans;
+    }
+};
