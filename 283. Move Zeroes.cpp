e27c33171aecae668c553e63390f595943de2182
Aman-Arcanion9 <46062838+Aman-Arcanion9@users.noreply.github.com>
2020-06-30 23:26:46 +0530

    Please

---
+++ 283. Move Zeroes.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int n = nums.size();
+        int count=0;
+        
+        for(auto i:nums)
+            if(i==0) count++;
+        
+        int j=0;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]!=0){
+                nums[j]=nums[i];
+                j++;
+            }   
+        }
+        
+        for( ; j<n ; j++)
+            nums[j]=0;
+    }
+};
